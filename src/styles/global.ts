5eb7652b915c28be41956bae7828b6573d56109d
Matheus Pires <matheushenriquepires99@gmail.com>
2021-03-24 18:27:39 -0300

    style: create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export default createGlobalStyle`
+  ${({ theme }) => css`
+    * {
+      margin: 0;
+      padding: 0;
+      outline: 0;
+      box-sizing: border-box;
+    }
+
+    html {
+      height: 100%;
+
+      @media (min-width: 1000px) {
+        font-size: 93.75%;
+      }
+
+      @media (min-width: 720px) {
+        font-size: 87.5%;
+      }
+    }
+
+    body {
+      height: 100%;
+      -webkit-font-smoothing: antialiased !important;
+      background: ${theme.colors.background};
+      color: ${theme.colors.text};
+    }
+
+    body,
+    input,
+    button,
+    textarea {
+      font: 400 1rem 'Roboto', sans-serif;
+    }
+
+    button {
+      cursor: pointer;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+  `}
+`;
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    white: '#ffffff',
+    blue: '#61dafb',
+    yellow: '#eba417',
+
+    text: '#e1e1e6',
+    background: '#121214',
+  },
+};
