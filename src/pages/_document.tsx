9e344723338115bedf49795330a554cf9590cd68
Matheus Pires <matheushenriquepires99@gmail.com>
2021-03-24 18:19:33 -0300

    chore: external fonts and dynamic title config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { DocumentInitialProps, DocumentContext } from 'next/document';
+import Document, {
+  DocumentInitialProps,
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 
 import { ServerStyleSheet } from 'styled-components';
 
@@ -28,5 +35,24 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
